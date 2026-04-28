7e5f8ef27bad460dd95880d8b4c007a8ba1a55b4
Marcelo Sekimoto <msekimoto@gmail.com>
2026-04-27 23:01:49 -0300

    Add AbilityService

---
+++ src/ServerScriptService/Services/AbilityService.server.lua
@@ -0,0 +1,15 @@
+local AbilityService = {}
+
+function AbilityService.UseAbility(player, abilityName)
+	print(player.Name .. " usou habilidade: " .. abilityName)
+
+	if abilityName == "BananaDistraction" then
+		-- lógica futura
+	end
+
+	if abilityName == "RoarStun" then
+		-- lógica futura
+	end
+end
+
+return AbilityService
