b1c074e47ef3b0851eb681ea9015163c021bd357
Marcelo Sekimoto <msekimoto@gmail.com>
2026-04-27 23:03:06 -0300

    Implement full ability system

--- src/ServerScriptService/Services/AbilityService.server.lua
+++ src/ServerScriptService/Services/AbilityService.server.lua
@@ -1,15 +1,1 @@
-local AbilityService = {}
-
-function AbilityService.UseAbility(player, abilityName)
-	print(player.Name .. " usou habilidade: " .. abilityName)
-
-	if abilityName == "BananaDistraction" then
-		-- lógica futura
-	end
-
-	if abilityName == "RoarStun" then
-		-- lógica futura
-	end
-end
-
-return AbilityService
+<FULL_REWRITE>
