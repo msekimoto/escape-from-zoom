655f8210f8505937cb544bbf5691ef72468c3562
Marcelo Sekimoto <msekimoto@gmail.com>
2026-04-27 23:01:29 -0300

    Add character config

---
+++ src/ReplicatedStorage/Shared/CharacterConfig.lua
@@ -0,0 +1,47 @@
+local Characters = {
+
+	Momo = {
+		Abilities = {
+			"ClimbBoost",
+			"BananaDistraction"
+		}
+	},
+
+	Porinha = {
+		Abilities = {
+			"HealPulse",
+			"ShieldBubble"
+		}
+	},
+
+	Leon = {
+		Abilities = {
+			"RoarStun",
+			"Dash"
+		}
+	},
+
+	Elly = {
+		Abilities = {
+			"Stomp",
+			"Knockback"
+		}
+	},
+
+	Snapper = {
+		Abilities = {
+			"Bite",
+			"WaterHide"
+		}
+	},
+
+	Grumblet = {
+		Abilities = {
+			"Scream",
+			"SlowFeathers"
+		}
+	}
+
+}
+
+return Characters
